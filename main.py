aa95ec5aa4548eb7d31a12474867a13c51ea5f68
Dmitriy-U <dmitriyushkalov@gmail.com>
2022-08-26 17:30:35 +0300

    Сделал тестовую обработку команд яндекс диалогов

--- main.py
+++ main.py
@@ -4,14 +4,48 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+commands = {
+    "включить": "on",
+    "выключить": "off"
+}
 
-@app.route("/", methods=["POST"])
+commands_list = list(commands.keys())
+
+
+@app.route("/", methods=["POST", "GET"])
 def alisa_command_handler():
     print("-->", request.json)
-    response = make_response(
-        {
-            "status": "ok"
-        },
-        200
-    )
-    return response
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    payload = request.json
+
+    if payload['session']['new']:
+        # Это новый пользователь.
+        response['response']['buttons'] = get_suggests()
+        response['response']['text'] = 'Привет! Что хочешь сделать с умной лампой Бастион?'
+        return make_response(response, 200)
+
+    command = payload['request']['original_utterance'].lower()
+
+    if command in commands_list:
+        response['response']['buttons'] = get_suggests()
+        response['response']['text'] = f'Хорошо {commands[command]}'
+        return make_response(response, 200)
+
+    response['response']['buttons'] = get_suggests()
+    response['response']['text'] = 'Уточните пожалуйста ...'
+    return make_response(response, 200)
+
+
+def get_suggests():
+    suggests = list()
+    for command in commands_list:
+        suggests.append({'title': command, 'hide': True})
+    print('suggests -->', suggests)
+    return suggests
